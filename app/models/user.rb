fc32e000a7108722b844c952c5aa76fc509221f2
Tangati, Lavanya <ltangati@paypal.com>
2017-03-13 14:49:45 -0500

    Assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
    before_save { self.email = email.downcase if email.present? }
+   before_save {
+     if name
+       combine_name = []
+       name.split.each do |nm|
+         combine_name << nm.capitalize
+       end
+       self.name = combine_name.join(" ")
+     end
+   }
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
    validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
