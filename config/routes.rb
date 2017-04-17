1954412479a7b0aa18479db0716c700ba5e60d97
Tangati, Lavanya <ltangati@paypal.com>
2017-04-17 13:57:59 -0500

    checkpoint 42 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :topics do
     resources :posts, except: [:index]
   end
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :labels, only: [:show]
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
