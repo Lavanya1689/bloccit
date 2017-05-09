905f55e9f7ecd20cdb64e40f135a4a107c135e98
Tangati, Lavanya <ltangati@paypal.com>
2017-05-09 11:40:06 -0500

    checkpoint 48 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418205723) do
+ActiveRecord::Schema.define(version: 20170508211505) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170418205723) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
